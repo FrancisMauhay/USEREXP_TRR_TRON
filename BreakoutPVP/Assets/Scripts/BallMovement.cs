574f9f4f67d7ce7736b43cac6d73c5b0623024a3
FrancisMauhay <francisemmanuel.mauhay@benilde.edu.ph>
2024-06-13 12:56:12 +0800

    Milestone 1(v0.2)
    - Changed the layout to a horizontal layout
    - Made sure that the Player inputs and ball movements were precise
    - Added in basic ball behavior
    - ball speeds up every successful hit

--- BreakoutPVP/Assets/Scripts/BallMovement.cs
+++ BreakoutPVP/Assets/Scripts/BallMovement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
+    [SerializeField] float moveSpeed;
+    private bool player2 = false;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        Launch();   
     }
 
-    // Update is called once per frame
-    void Update()
+    void Launch()
     {
-        
+        if (rb != null)
+        {
+            if (player2 == false)
+            {
+                rb.velocity = Vector2.left * moveSpeed;
+            }
+            else if(player2 == true)
+            {
+                rb.velocity = Vector2.right * moveSpeed;
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerMovement>() != null)
+        {
+            Vector2 hitFactor = CalculateHitFactor(transform.position, collision.transform.position, collision.collider.bounds.size);
+            Vector2 newDirection = new Vector2(hitFactor.x, hitFactor.y).normalized;
+            rb.velocity = newDirection * moveSpeed;
+            moveSpeed++;
+        }
+    }
+
+    Vector2 CalculateHitFactor(Vector2 ballPos, Vector2 paddlePos, Vector2 paddleSize)
+    {
+        float x = (ballPos.x - paddlePos.x) / paddleSize.x;
+        float y = (ballPos.y - paddlePos.y) / paddleSize.y;
+        return new Vector2(x, y);
     }
 }
